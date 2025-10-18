5c6fa62865d0cddbd66909fd63fc2f19122bd01d
kinoubenkyou <kinoubenkyou@gmail.com>
2025-10-18 17:43:09 +0700

    update valid-palindrome

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -1,6 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        normalized: str = "".join(char for char in s if char.isalnum()).lower()
-        length = len(normalized)
-        half_length = length // 2
-        return normalized[:half_length] == normalized[length - 1:length - 1 - half_length:-1]
+        normalized: list[int] = []
+        for char in s:
+            code_point = ord(char)
+            if 65 <= code_point <= 90:
+                normalized.append(code_point + 32)
+            elif 48 <= code_point <= 57 or 97 <= code_point <= 122:
+                normalized.append(code_point)
+        for index in range(0, len(normalized) // 2):
+            if normalized[index] != normalized[len(normalized) - 1 - index]:
+                return False
+        return True
