94a9d28c241f4392193760b8f93852df5c44c4b6
kinoubenkyou <kinoubenkyou@gmail.com>
2025-10-22 15:46:43 +0700

    update invert-binary-tree

--- invert-binary-tree.py
+++ invert-binary-tree.py
@@ -6,8 +6,11 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root:
-            root.left, root.right = root.right, root.left
-            self.invertTree(root.left)
-            self.invertTree(root.right)
+        stack: list[TreeNode] = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack.append(node.left)
+                stack.append(node.right)
         return root
