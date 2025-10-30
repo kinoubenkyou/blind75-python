6c877e071a908ac3744b971051fe9c5dd116b3cd
kinoubenkyou <kinoubenkyou@gmail.com>
2025-10-30 11:45:36 +0700

    update ransom-note, longest-palindrome

--- longest-palindrome.py
+++ longest-palindrome.py
@@ -1,13 +1,13 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        counts = [0] * 58
+        frequencies = [0] * 58
         for character in s:
-            counts[ord(character) - 65] += 1
+            frequencies[ord(character) - 65] += 1
         length, middle_length = 0, 0
-        for count in counts:
-            if count == 0:
+        for frequency in frequencies:
+            if frequency == 0:
                 continue
-            elif count % 2 == 1:
+            elif frequency % 2 == 1:
                 middle_length = 1
-            length += count // 2
+            length += frequency // 2
         return length * 2 + middle_length
--- ransom-note.py
+++ ransom-note.py
@@ -1,12 +1,12 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        count_by_character: list[int] = [0] * 26
+        frequencies = [0] * 26
         for character in magazine:
-            count_by_character[ord(character) - 97] += 1
+            frequencies[ord(character) - 97] += 1
         for character in ransomNote:
-            index = ord(character) - 97
-            if count_by_character[index] > 0:
-                count_by_character[index] -= 1
+            code_point = ord(character) - 97
+            if frequencies[code_point] > 0:
+                frequencies[code_point] -= 1
             else:
                 return False
         return True
